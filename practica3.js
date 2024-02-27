252b2adfbd1fb835293d85b8ad1129c44ea7ae0a
FPA2000 <78817730+FPA2000@users.noreply.github.com>
2024-02-27 13:42:49 +0100

    subida practica

--- practica3.js
+++ practica3.js
@@ -1,64 +1,31 @@
-
-// function getPlant(q){
-//   $.ajax({
-//     url:"https://trefle.io/api/v1/plants/search?token=VldyjaIcaB83LyYTcMQuBBIe0YfvefwNoDYkfQdfhr0&q=${q}",
-//     success: function(res) {
-
-//       document.getElementById("output").innerHTML="<br><br><h1>"+res.data[0].common_name+" - "
-//       +res.data[0].scientific_name+"</h1><br><img src='"
-//       +res.data[0].image_url+"' /><br>Descubierto en el año: "
-//       +res.data[0].year + "por: "+res.data[0].author
-//     },
-//     error: function(xhr, status, error) {
-//       console.error("Error en la solicitud AJAX:", error);
-//       document.getElementById("output").innerHTML = "Se produjo un error al cargar los datos.";
-//     }
-//   });
-// }
-
-// async function getPlant(q) {
-//   try {
-//     const response = await fetch(`http://trefle.io/api/v1/plants/search?token=VldyjaIcaB83LyYTcMQuBBIe0YfvefwNoDYkfQdfhr0&q=${q}`);
-//     // if (!response.ok) {
-//     //   throw new Error('La solicitud a la API falló');
-//     // }
-//     const json = await response.json();
-
-//     if (json.data && json.data.length > 0) {
-//       const plant = json.data[0];
-//       document.getElementById("output").innerHTML = `<br><br><h1>${plant.common_name} - ${plant.scientific_name}</h1><br><img src='${plant.image_url}' /><br>Descubierto en el año: ${plant.year} por: ${plant.author}`;
-//     } else {
-//       document.getElementById("output").innerHTML = "No se encontraron resultados.";
-//     }
-//   } catch (error) {
-//     console.error("Error en la solicitud:", error);
-//     document.getElementById("output").innerHTML = "Se produjo un error al cargar los datos.";
-//   }
-// }
-
-function getPlant(q){
-  $.ajax({
-    url:`https://trefle.io/api/v1/plants/search?token=VldyjaIcaB83LyYTcMQuBBIe0YfvefwNoDYkfQdfhr0&q=${q}`,
-    success: function(res) {
-
-      document.getElementById("output").innerHTML="<br><br><h1>"+res.data[0].common_name+" - "
-      +res.data[0].scientific_name+"</h1><br><img src='"
-      +res.data[0].image_url+"' /><br>Descubierto en el año: "
-      +res.data[0].year + "por: "+res.data[0].author
-    },
-    error: function(xhr, status, error) {
-      console.error("Error en la solicitud AJAX:", error);
-      document.getElementById("output").innerHTML = "Se produjo un error al cargar los datos.";
+async function getPlant(q) {
+  try {
+    const proxyUrl = 'https://api.allorigins.win/get?url='
+    const apiUrl = 'https://trefle.io/api/v1/plants/search?token=VldyjaIcaB83LyYTcMQuBBIe0YfvefwNoDYkfQdfhr0&q=' + q
+    const response = await fetch(proxyUrl + encodeURIComponent(apiUrl))
+    if (!response.ok) {
+      throw new Error('La solicitud a la API falló');
     }
-  });
+    const json = await response.json();
+    const nestedData = JSON.parse(json.contents)
+
+    if (nestedData.data && nestedData.data.length > 0) {
+      const plant = nestedData.data[0];
+      document.getElementById("output").innerHTML = `<br><br><h1>${plant.common_name} - ${plant.scientific_name}</h1><br><img src='${plant.image_url}' /><br>Descubierto en el año: ${plant.year} por: ${plant.author}`;
+    } else {
+      document.getElementById("output").innerHTML = "No se encontraron resultados.";
+    }
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    document.getElementById("output").innerHTML = "Se produjo un error al cargar los datos.";
+  }
 }
 
 
-
-function validateUser(user,pass) {
-  if (correo.length!=0 && pass.length!=0){
+function validateUser(email, pass) {
+  if (email.length!=0 && pass.length!=0 && email.includes('@')){
     alert('Se ha suscrito con éxito. Recibirá un correo cada mes desde el cual podrá acceder a la revista.');
   }else{
-    alert('Introduzca valores en todos los campos.');
+    alert('Introduzca valores en todos los campos y asegurese de que la dirección de correo es válida.');
   }
 }
