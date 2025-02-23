885a0475fdc0bac298eeedbfbd0fa5e4de0a04b9
Fernando Perez Alba <139076609+fernandoperezalba@users.noreply.github.com>
2025-02-23 18:32:53 +0100

    Update practica3.js actualizado modelo tras limite

--- practica3.js
+++ practica3.js
@@ -1,6 +1,6 @@
 async function getPlant(q) {
   try {
-      const apiKey = "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"; // Replace with your actual API key
+      const apiKey = "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"; // Replace with your actual API key
       const apiUrl = "https://openrouter.ai/api/v1/chat/completions";
 
       const response = await fetch(apiUrl, {
@@ -10,7 +10,7 @@ async function getPlant(q) {
               "Content-Type": "application/json",
           },
           body: JSON.stringify({
-              "model": "cognitivecomputations/dolphin3.0-r1-mistral-24b:free",
+              "model": "mistralai/mistral-small-24b-instruct-2501:free",
               "messages": [
                   { "role": "user", "content": `Give me a plant related short answer in spanish to the question: "${q}". Format the response in aesthetic HTML with clear headings (<h2>), subheadings (<h3>), and lists (<ul>) where appropriate, but do NOT include any code blocks, markdown formatting, or backticks.` }
                   // { "role": "user", "content": `Give me a plant related very short answer in spanish to the question: "${q}". Format the response in HTML` }
